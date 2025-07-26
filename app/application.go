5dbe9d34cccc2d2b6c9dad508d37aa27b3600233
m4nu-git <manudev912@gmail.com>
2025-07-26 13:59:12 +0530

    Added env file

--- app/application.go
+++ app/application.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	config "AuthInGo/config/env"
 	"fmt"
 	"net/http"
 	"time"
@@ -8,19 +9,36 @@ import (
 
 // Config holds the configuration for the server
 type Config struct {
-	Addr string
+	Addr string //PORT
 }
 
 type Application struct {
 	Config Config
 }
 
+// Constructor for config
+func NewConfig() Config {
+
+	port := config.GetString("PORT", ":8080")
+	
+	return Config{
+		Addr: port,
+	}
+}
+
+// Constructor for Application
+func NewApplication(cfg Config) *Application {
+	return &Application {
+		Config: cfg,
+	}
+}
+
 func (app *Application) Run() error {
 	server := &http.Server{
 		Addr: app.Config.Addr,
 		Handler: nil, // TODO: Setup a chi router and put it here
-		ReadTimeout: 10 * time.Second, //
-		WriteTimeout: 10 * time.Second, //
+		ReadTimeout: 10 * time.Second, // Set read timeout to 10 seconds
+		WriteTimeout: 10 * time.Second, // Set write timeout to 10 seconds
 	}
 
 	fmt.Println("Starting server on", app.Config.Addr)
--- main.go
+++ main.go
@@ -1,16 +1,23 @@
 package main
 
-import "AuthInGo/app"
+import (
+	"AuthInGo/app"
+)
 
 func main() {
 
-	cfg := app.Config {
-		Addr: ":3001",
-	}
+	// cfg := app.Config {
+	// 	Addr: ":3001",
+	// }
 
-	app := app.Application {
-		Config: cfg,
-	}
+	// app := app.Application {
+	// 	Config: cfg,
+	// }
+
+
+
+	cfg := app.NewConfig() // Set the server to listen on port 8080
+	app := app.NewApplication(cfg)
 
 	app.Run()
 }
